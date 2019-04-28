a409c0b860969b1a1a5f221bc0c32c7baf0ee425
Eddy Leon <32406542+eleon479@users.noreply.github.com>
2019-04-28 05:34:20 -0400

    Empty directory request redirects to login for now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'welcome', component: WelcomeComponent },
-  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' }
 ];
 
